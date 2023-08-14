eabad73d1e887a55720858be50dca9f1445edd4a
Pro-fish0 <mnsxmns@gmail.com>
2023-08-14 21:10:51 +0300

    v0.1

---
+++ frontend.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def create_gui():
+    window = tk.Tk()
+    window.title("Meme Generator")
+
+    # Add widgets: buttons, labels, entry fields
+    # Logic to open image, get text, and call `generate_meme`
+
+    window.mainloop()
+
+create_gui()
