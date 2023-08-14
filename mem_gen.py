1e611c7e087bc4808410700115a93901e1ea94c1
Pro-fish0 <mnsxmns@gmail.com>
2023-08-15 01:49:55 +0300

    better ui

--- mem_gen.py
+++ mem_gen.py
@@ -1,50 +1,93 @@
-import tkinter as tk
-from tkinter import filedialog, simpledialog, colorchooser
-from PIL import Image, ImageTk
+import sys
 import requests
-from io import BytesIO
-import giphy_client
-from giphy_client.rest import ApiException
+import threading
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel, QScrollArea, QGridLayout, QFrame
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, pyqtSignal
 
-# Initialize GIPHY client
-api_instance = giphy_client.DefaultApi()
-API_KEY = 'LWoWtvGY30VXXMlN5ScXo2YnKSRDkDnI'  # Replace this with your GIPHY API key
+# GIPHY API key
+GIPHY_API_KEY = 'API_KEY'
+GIPHY_ENDPOINT = 'https://api.giphy.com/v1/gifs/search'
 
-def search_giphy(tag, limit=10):
-    try:
-        response = api_instance.gifs_search_get(API_KEY, tag, limit=limit)
-        return [x.images.original.url for x in response.data]
-    except ApiException as e:
-        print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
-        return []
+class ImageSearchApp(QWidget):
+    imageFetchedSignal = pyqtSignal(str, QPixmap)
 
-def fetch_image(url):
-    response = requests.get(url)
-    return Image.open(BytesIO(response.content))
+    def __init__(self):
+        super().__init__()
 
-def create_gui():
-    window = tk.Tk()
-    window.title("M3m3 Generator")
+        self.initUI()
+        self.imageFetchedSignal.connect(self.onImageFetched)
+        self.image_count = 0  # Add this line to initialize the counter
 
-    def search_meme_template():
-        tag = simpledialog.askstring("Search", "Enter meme template tag:")
-        if tag:
-            urls = search_giphy(tag)
-            if urls:
-                # For simplicity, we'll just fetch the first meme template
-                # In a more advanced version, you can provide a gallery for the user to choose from
-                img = fetch_image(urls[0])
-                img.thumbnail((300, 300))
-                img_tk = ImageTk.PhotoImage(img)
-                preview_label.config(image=img_tk)
-                preview_label.image = img_tk
+    def initUI(self):
+        layout = QVBoxLayout()
 
-    search_button = tk.Button(window, text="Search Meme Template", command=search_meme_template)
-    search_button.pack(pady=20)
+        self.tag_input = QLineEdit(self)
+        self.tag_input.setPlaceholderText("Enter meme template tag")
+        layout.addWidget(self.tag_input)
 
-    preview_label = tk.Label(window)
-    preview_label.pack(pady=20)
+        search_button = QPushButton("Search", self)
+        search_button.clicked.connect(self.show_meme_templates)
+        layout.addWidget(search_button)
 
-    window.mainloop()
+        self.scroll_area = QScrollArea(self)
+        self.scroll_area.setWidgetResizable(True)
+        layout.addWidget(self.scroll_area)
 
-create_gui()
+        self.image_grid = QGridLayout()
+        self.image_frame = QFrame()
+        self.image_frame.setLayout(self.image_grid)
+        self.scroll_area.setWidget(self.image_frame)
+
+        self.setLayout(layout)
+
+    def search_giphy(self, tag, limit=10):
+        params = {
+            'api_key': GIPHY_API_KEY,
+            'q': tag,
+            'limit': limit
+        }
+        response = requests.get(GIPHY_ENDPOINT, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return [img['images']['original']['url'] for img in data['data']]
+        else:
+            print(f"Error fetching from Giphy: {response.text}")
+            return []
+
+    def fetch_image(self, url):
+        response = requests.get(url)
+        pixmap = QPixmap()
+        pixmap.loadFromData(response.content)
+        self.imageFetchedSignal.emit(url, pixmap)
+
+    def show_meme_templates(self):
+        tag = self.tag_input.text()
+        urls = self.search_giphy(tag)
+
+        # Clear previous items
+        for i in reversed(range(self.image_grid.count())): 
+            widget = self.image_grid.itemAt(i).widget()
+            widget.deleteLater()
+
+        # Fetch images in separate threads
+        for url in urls:
+            threading.Thread(target=self.fetch_image, args=(url,)).start()
+
+    def onImageFetched(self, url, pixmap):
+        label = QLabel(self)
+        label.setPixmap(pixmap.scaled(200, 200, Qt.KeepAspectRatio))
+        
+        row = self.image_count // 3
+        col = self.image_count % 3
+
+        self.image_grid.addWidget(label, row, col)
+        self.image_count += 1  # Increment the counter
+
+app = QApplication(sys.argv)
+window = ImageSearchApp()
+window.setWindowTitle('Meme Template Search')
+window.resize(650, 300)
+window.show()
+sys.exit(app.exec_())
+
